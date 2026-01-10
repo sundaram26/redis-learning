5bc3bf5e5a262e6d98c646d66d6990ac08def363
sundaram26 <singhsundaram2004@gmail.com>
2026-01-10 22:58:56 +0530

    Updated

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.get('/', async (req, res) => {
     const cacheValue = await client.get('todos');
     if (cacheValue) return res.json(JSON.parse(cacheValue));
     const { data } = await axios.get("https://jsonplaceholder.typicode.com/todos"); 
-    await client.set('todos', JSON.stringify(data));
+    await client.set('todos', JSON.stringify(data))
     await client.expire('todos', 30);
     return res.json(data);
 })
